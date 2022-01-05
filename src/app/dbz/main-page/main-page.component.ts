5b398fb5f1f9edaff879b5d1cb8d911d09b710e1
Kevin Barreto <kevyyar@gmail.com>
2022-01-04 20:41:11 -0600

    added ngModel to inputs

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Character {
+  name: string;
+  powerLvl: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-  send() {
-    console.log('this is a test');
+  newChar: Character = {
+    name: 'Trunks',
+    powerLvl: 14000
   }
+
+  send() {
+    console.log(this.newChar);
+  }
+
 }
